e9f246a9ac75c00f6cd2753a6f324af5020a91d3
dangcuong22 <33875592+dangcuong22@users.noreply.github.com>
2020-02-20 23:56:07 +0700

    Revert "init"
    This reverts commit 8f43b8f15a38d4c9712fbc4fbfa7ffebbf81bc65.

--- src/socket.js
+++
@@ -1,6 +0,0 @@
-socket = require('socket.io-client')(process.env.socket_server);
-socket.on('connect',()=> console.log("Socket connected "+process.env.socket_server))
-
-module.exports = {
-    socket: socket
-};
--- src/utils/range.js
+++
@@ -1,6 +0,0 @@
-const range = (s, e) => Array(e + 1 - s).fill(s).map((e, i) => i + s);
-
-
-module.exports={
-    range
-}
